3717d39e19a639f0b4653bea88e74c4463144b17
정현우 <erios5774@gmail.com>
2025-03-07 10:04:43 +0900

    Update fsm_mealy.v

--- 0306/fsm_mealy.v
+++ 0306/fsm_mealy.v
@@ -3,88 +3,136 @@
 module fsm_mealy(
     input clk,
     input rst,
-    input in_bit,
-    output reg out_bit
+    input [2:0] sw,
+    output [2:0] led
     );
 
+    reg [2:0] r_led;
     reg [2:0] state, next;
 
-    // 상태 정의
-    parameter [2:0] start = 3'b000,
-                    rd0_once = 3'b001,
-                    rd1_once = 3'b010,
-                    rd0_twice = 3'b011,
-                    rd1_twice = 3'b100;
+    assign led = r_led;
 
-    // 1. 상태 레지스터 업데이트
-    always @ (posedge clk or posedge rst) begin
-        if (rst)
-            state <= start;
-        else
+    parameter [2:0] idle = 3'b000,
+                    st1 = 3'b001,
+                    st2 = 3'b010,
+                    st3 = 3'b100,
+                    st4 = 3'b111;
+
+    always @ (posedge clk, posedge rst) begin
+        if (rst) begin            
+            state <= 0;
+        end else begin            
             state <= next;
+        end
     end
 
-    // 2. 다음 상태 결정
+    // for continue next state
+        
     always @ (*) begin
-        next = state;
+        
+        next = state;    //    initial value -> if not exist, LATCH 
 
-        case (state)
-            start: begin
-                if (in_bit == 0) 
-                    next = rd0_once;
-                else if (in_bit == 1) 
-                    next = rd1_once;
+        case (state)            
+            idle : begin                
+                if (sw == 3'b001) begin
+                    next = st1;
+                end else if (sw == 3'b010) begin
+                    next = st2;
+                end 
             end
 
-            rd0_once: begin
-                if (in_bit == 0)
-                    next = rd0_twice;  // 0이 반복될 경우
-                else if (in_bit == 1)
-                    next = rd1_once;  // 1이 들어오면 rd1_once로 이동
+            st1 : begin
+                if (sw == 3'b010) begin
+                    next = st2;
+                end
             end
 
-            rd0_twice: begin
-                if (in_bit == 0)
-                    next = rd0_twice;  // 0이 계속 들어오면 계속 rd0_twice
-                else if (in_bit == 1)
-                    next = rd1_once;  // 1이 들어오면 rd1_once로 이동
+            st2 : begin
+                if (sw == 3'b100) begin
+                    next = st3;
+                end
             end
 
-            rd1_once: begin
-                if (in_bit == 1)
-                    next = rd1_twice;  // 1이 반복될 경우
-                else if (in_bit == 0)
-                    next = rd0_once;  // 0이 들어오면 rd0_once로 이동
+            st3 : begin
+                if (sw == 3'b000) begin
+                    next = idle;
+                end else if (sw == 3'b001) begin
+                    next = st1;
+                end else if (sw == 3'b111) begin
+                    next = st4;
+                end
             end
 
-            rd1_twice: begin
-                if (in_bit == 1)
-                    next = rd1_twice;  // 1이 계속 들어오면 계속 rd1_twice
-                else if (in_bit == 0)
-                    next = rd0_once;  // 0이 들어오면 rd0_once로 이동
+            st4 : begin
+                if (sw == 3'b100) begin
+                    next = st3;
+                end
             end
-
-            default: next = start;
+            
+            default: begin
+                next = state;
+            end
+            
         endcase
     end
 
-    // 3. 출력 결정
+    // output combinational logic
+
     always @ (*) begin
-        out_bit = 1'b0;  // 기본 출력은 0
-
+        
+        r_led = 0;
+        
         case (state)
-            rd0_once, rd0_twice: begin
-                if (in_bit == 0)
-                    out_bit = 1'b1;  // 0이 반복되면 out_bit = 1
+            idle : begin            
+//              r_led = 3'b000;                     moore model
+                if (sw == 3'b001) begin          // mealy model affect by input sw
+                    r_led = 3'b001; 
+                end else if (sw == 3'b010) begin
+                    r_led = 3'b010;
+                end else begin
+                    r_led = 3'b000;             // no affection default
+                end
             end
-
-            rd1_once, rd1_twice: begin
-                if (in_bit == 1)
-                    out_bit = 1'b1;  // 1이 반복되면 out_bit = 1
+            
+            st1 : begin
+                if (sw === 3'b010) begin
+                    r_led = 3'b010;
+                end else begin
+                    r_led = 3'b001;
+                end
             end
-
-            default: out_bit = 1'b0;  // 기본은 0
+            
+            st2 : begin
+                if (sw == 3'b100) begin
+                    r_led = 3'b100;
+                end else begin
+                    r_led = 3'b010;
+                end
+            end
+            
+            st3 : begin
+                if (sw == 3'b000) begin
+                    r_led = 3'b000;
+                end else if (sw == 3'b001) begin
+                    r_led = 3'b001;
+                end else if (sw == 3'b111) begin
+                    r_led = 3'b111;
+                end else begin
+                    r_led = 3'b100;
+                end
+            end
+            
+            st4 : begin
+                if (sw == 3'b100) begin
+                    r_led = 3'b100;
+                end else begin
+                    r_led = 3'b111;
+                end
+            end
+            
+            default : begin
+                r_led = 3'b000;
+            end
         endcase
     end
-
 endmodule
